f29ba15a219752fec4b2feb4d5658e87ad755cd6
HelloWorldSungin <sik960114@gmail.com>
2026-07-22 20:05:14 -0700

    fix(nvim): remove deprecated nvim-treesitter.configs requirement

--- config/nvim/lua/plugins/markdown.lua
+++ config/nvim/lua/plugins/markdown.lua
@@ -1,17 +1,12 @@
 -- Markdown rendering and Treesitter parsing
 return {
   {
-    -- Treesitter: mandatory for render-markdown and syntax highlighting.
+    -- Treesitter: syntax highlighting and AST parser generator.
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
-    lazy = false,
     opts = {
       ensure_installed = { "markdown", "markdown_inline", "lua", "vim", "bash", "json", "yaml" },
-      highlight = { enable = true },
     },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-    end,
   },
   {
     -- Render Markdown: pretty-view markdown files in Neovim instead of raw text.
