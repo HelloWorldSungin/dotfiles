86cde2c6941269225eda6807f244b94c11d994b8
HelloWorldSungin <sik960114@gmail.com>
2026-07-30 22:13:09 -0700

    feat(nvim): add ]c and [c keymaps in gitsigns for jumping between git changes

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -12,11 +12,38 @@ return {
     },
   },
   {
-    -- Gitsigns: change markers in the gutter + inline blame on the current line.
+    -- Gitsigns: change markers in the gutter + inline blame + change navigation.
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
     opts = {
       current_line_blame = true,
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Jump to next/previous git change (hunk)
+        map("n", "]c", function()
+          if vim.wo.diff then return "]c" end
+          vim.schedule(function() gs.next_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Next git change" })
+
+        map("n", "[c", function()
+          if vim.wo.diff then return "[c" end
+          vim.schedule(function() gs.prev_hunk() end)
+          return "<Ignore>"
+        end, { expr = true, desc = "Previous git change" })
+
+        -- Git change actions
+        map("n", "<leader>hp", gs.preview_hunk, { desc = "Preview git change hunk" })
+        map("n", "<leader>hs", gs.stage_hunk, { desc = "Stage git change hunk" })
+        map("n", "<leader>hr", gs.reset_hunk, { desc = "Reset git change hunk" })
+      end,
     },
   },
 }
